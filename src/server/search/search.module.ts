59e439721f2b5ed4e7db2d0e94f9aa73a6a85375
mattwhitfield <whitfield.mj@gmail.com>
2022-12-21 09:36:42 +0000

    change uplaod key to include trigger-pipeline/

--- src/server/search/search.module.ts
+++ src/server/search/search.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { HttpModule } from '@nestjs/axios';
 import { DataModule } from '../data/data.module';
-import { TnaDal } from '../data/tna.dal';
 
 @Module({
   imports: [HttpModule, DataModule],
