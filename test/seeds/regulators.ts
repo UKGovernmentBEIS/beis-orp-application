78bc667e2ee2ab375d6edbb25c2ca4871692b7db
mattwhitfield <whitfield.mj@gmail.com>
2023-01-18 16:00:16 +0000

    add postgres instance to e2e tests in pipeline

---
+++ test/seeds/regulators.ts
@@ -0,0 +1,15 @@
+export const REGULATOR_UUID = '9d49ea3e-67d5-4985-90b0-e184ddc59d05';
+const REGULATOR_2_UUID = '6a7a4ee1-fd7e-4e44-a3cb-1f2e9a71194e';
+
+export const regulators = [
+  {
+    name: 'Regulator',
+    domain: 'regulator.com',
+    apiKey: REGULATOR_UUID,
+  },
+  {
+    name: 'Regulator 2',
+    domain: 'regulator2.tech',
+    apiKey: REGULATOR_2_UUID,
+  },
+];
