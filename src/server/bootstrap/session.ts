a2782c2c64f2750d45f8358f9a012eb75dd165f7
mattwhitfield <whitfield.mj@gmail.com>
2023-04-18 10:55:46 +0100

    add auto secure flag to cookies

--- src/server/bootstrap/session.ts
+++ src/server/bootstrap/session.ts
@@ -22,7 +22,7 @@ export function useSession(app: NestExpressApplication) {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: config.get('isProduction'),
+        secure: 'auto',
       },
     }),
   );
