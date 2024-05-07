435110d946a39a589592cb869c8fd8aca832870e
DineshKumarASD <118263966+DineshKumarASD@users.noreply.github.com>
2024-05-07 15:10:39 +0100

    Update regulators.ts

--- src/server/regulator/config/regulators.ts
+++ src/server/regulator/config/regulators.ts
@@ -331,6 +331,11 @@ const regulators: Regulator[] = [
     name: 'Safety Tech Accelerator',
     domain: 'lr.org',
   },
+     {
+    id: 'gmail',
+    name: 'gmail test',
+    domain: 'gmail.com',
+  },
   {
     id: 'Cognizant',
     name: 'Cognizant test',
