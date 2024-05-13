81e120ce9e7250f39e13b7118163894d35b38434
DineshKumarASD <118263966+DineshKumarASD@users.noreply.github.com>
2024-05-13 13:15:34 +0100

    Update regulators.ts

--- src/server/regulator/config/regulators.ts
+++ src/server/regulator/config/regulators.ts
@@ -332,6 +332,12 @@ const regulators: Regulator[] = [
     domain: 'lr.org',
   },
   {
+    id: 'gmail',
+    name: 'gmail test',
+    domain: 'gmail.com',
+  },
+
+  {
     id: 'Cognizant',
     name: 'Cognizant test',
     domain: 'cognizant.com',
